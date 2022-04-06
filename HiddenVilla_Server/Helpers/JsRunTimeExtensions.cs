06c10ee751cfc1434a60603c30d127f40fa84d77
ashkan semsar <ashkansemsar@hotmail.com>
2022-04-06 11:59:19 +0430

    call child component method

---
+++ HiddenVilla_Server/Helpers/JsRunTimeExtensions.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.JSInterop;
+
+namespace HiddenVilla_Server.Helpers
+{
+    public static class JsRunTimeExtensions
+    {
+        public static async ValueTask ShowErrorToaster(this IJSRuntime jsRuntime, string message)
+        {
+           await jsRuntime.InvokeVoidAsync("ShowToaster","error", message);
+        }
+        public static async ValueTask ShowSuccessToaster(this IJSRuntime jsRuntime, string message)
+        {
+            await jsRuntime.InvokeVoidAsync("ShowToaster", "success", message);
+        }
+        public static async ValueTask ShowWarningToaster(this IJSRuntime jsRuntime, string message)
+        {
+            await jsRuntime.InvokeVoidAsync("ShowToaster", "warning", message);
+        }
+        public static async ValueTask ShowInfoToaster(this IJSRuntime jsRuntime, string message)
+        {
+            await jsRuntime.InvokeVoidAsync("ShowToaster", "info", message);
+        }
+        public static async ValueTask ShowSuccessSweetAlert(this IJSRuntime jsRuntime, string message)
+        {
+            await jsRuntime.InvokeVoidAsync("ShowSweetAlert", "success", message);
+        }
+    }
+}
--- HiddenVilla_Server/wwwroot/css/myScript.js
+++ HiddenVilla_Server/wwwroot/css/myScript.js
@@ -4,7 +4,21 @@
         
     }
     if (type === "error") {
-        toastr.success("This is Error", "Operation Result");
+        toastr.error("This is Error", "Operation Result");
+    }
+    if (type === "warning") {
+        toastr.warning("This is warning", "Operation Result");
+    }
+    if (type === "info") {
+        toastr.info("This is info", "Operation Result");
     }
 
+}
+window.ShowSweetAlert = (type, message) => {
+
+    Swal.fire(
+        'Good job!',
+        'You clicked the button!',
+        'success'
+    );
 }
